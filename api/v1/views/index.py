b2555329c21cd0a9ea9f78e2e9c4cb7b1d8ed7c8
patmuchiri <pmuchirig@gmail.com>
2024-04-01 13:06:59 -0700

    some stats an endpoint that retrieves the number of each objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,7 @@
 """ The status route """
 
 from flask import jsonify, Blueprint
+from models import storage
 from api.v1.views import app_views
 
 
@@ -10,3 +11,20 @@ from api.v1.views import app_views
 def get_status():
     """ returns the status of the route """
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats", methods=['GET'], strict_slashes=False)
+def get_stats():
+    """ retrieves the number of each object by type """
+    objects = {
+            "amenities": "Amenity",
+            "cities": "City",
+            "places": "Place",
+            "reviews": "Review",
+            "states": "State",
+            "users": "User"
+            }
+    for key, value in object.items():
+        objects[key] = storage.count(value)
+
+    return jsonify(objects)
